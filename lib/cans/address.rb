dc8dfe29061074ca50e3910efdbfc5b2c7880d97
Bryce Kerley <bkerley@brycekerley.net>
2010-10-24 20:13:06 -0400

    decode addresses to methods

---
+++ lib/cans/address.rb
@@ -0,0 +1,47 @@
+module Cans
+  class Address
+    METHOD_KIND_REGEX = %r{\.(.)}
+    MODULE_NAME_REGEX = %r{^(.+)/\.}
+    METHOD_NAME_REGEX = %r{\../(.+)$}
+
+    def initialize(address_string)
+      @string = address_string
+    end
+
+    def module_name
+      md = MODULE_NAME_REGEX.match @string
+      raise "No module_name found in #{@string.inspect}" unless md
+      return md[1].gsub('/','::')
+    end
+
+    def method_kind
+      md = METHOD_KIND_REGEX.match @string
+      raise "No method_kind found" unless md
+      return case md[1]
+             when 'i'
+               :instance
+             when 'm'
+               :module
+             else
+               raise 'Unexpected method_kind found'
+             end
+    end
+
+    def method_name
+      md = METHOD_NAME_REGEX.match @string
+      raise 'No method_name found' unless md
+      return md[1].gsub('/','')
+    end
+
+    def target_method
+      mod = eval "::#{module_name}"
+      met = case method_kind
+            when :instance
+              mod.instance_method method_name
+            when :module
+              mod.method method_name
+            end
+      return met
+    end
+  end
+end
---
+++ test/test_address.rb
@@ -0,0 +1,26 @@
+require 'helper'
+
+class TestAddress < Test::Unit::TestCase
+  context 'an Address to an instance method on the Beverage class' do
+    setup do
+      @address = Cans::Address.new 'Beverage/.i/refreshing'
+    end
+    subject { @address }
+
+    should 'decode a module_name' do
+      assert_equal 'Beverage', subject.module_name
+    end
+
+    should 'decode a method_kind' do
+      assert_equal :instance, subject.method_kind
+    end
+
+    should 'decode a method_name' do
+      assert_equal 'refreshing', subject.method_name
+    end
+
+    should 'find the target_method' do
+      assert_equal Beverage.instance_method(:refreshing), subject.target_method
+    end
+  end
+end
