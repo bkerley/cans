53a4606a720aff397f648618458ca489f63885c3
Bryce Kerley <bkerley@brycekerley.net>
2010-10-24 20:15:04 -0400

    working method source

--- lib/cans/application.rb
+++ lib/cans/application.rb
@@ -1,6 +1,14 @@
 module Cans
-  class Application
-    def call(environment)
+  class Application < Sinatra::Base
+      set :views, File.dirname(__FILE__) + '/views'
+
+    get '/' do
+      'ok'
+    end
+
+    get '/method/*' do
+      @address = Address.new(params[:splat].first)
+      haml :method
     end
   end
 end
