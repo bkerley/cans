75d376012e531fa7ffb0946209140c06e4ca11fa
Bryce Kerley <bkerley@brycekerley.net>
2010-10-24 20:53:19 -0400

    inspect modules

--- lib/cans/application.rb
+++ lib/cans/application.rb
@@ -6,6 +6,20 @@ module Cans
       'ok'
     end
 
+    get '/module/*' do
+      @address = Address.new(params[:splat].first)
+      @module = @address.target_module
+
+      @local_instance_methods = @module.instance_methods false
+      @all_instance_methods = @module.instance_methods true
+      @super_instance_methods = @all_instance_methods - @local_instance_methods
+
+      @class_methods = @module.methods
+
+      @ancestors = @module.ancestors
+      haml :module
+    end
+
     get '/method/*' do
       @address = Address.new(params[:splat].first)
       haml :method
