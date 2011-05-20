9ca3e6b990c02f839ecb527da4dbc6693cc74e92
Bryce Kerley <bkerley@brycekerley.net>
2011-05-20 14:37:11 -0400

    make sure the templates are written to the test

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -2,4 +2,7 @@ require('/json2.js');
 require('/jquery-1.4.4.min.js');
 require('/underscore-min.js');
 require('/backbone-min.js');
+
+document.write("<div id='module_template'> \n</div> \n<div id='method_template'> \n</div> ");
+
 require('/application.js');
